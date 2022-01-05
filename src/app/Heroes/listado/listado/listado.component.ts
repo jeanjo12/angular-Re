cb2cb377cbd9951bf7e25c312e7232161eedefce
jeanjo12 <jeanollarves62@gmail.com>
2022-01-05 14:21:54 -0400

    1 file modified

--- src/app/Heroes/listado/listado/listado.component.ts
+++ src/app/Heroes/listado/listado/listado.component.ts
@@ -4,4 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-listado',
   templateUrl: 'listado.component.html',
 })
-export class ListadoComponent {}
+export class ListadoComponent {
+  heroe: string[] = [];
+}
